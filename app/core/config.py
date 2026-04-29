ad40e71b4516bb33c7d2b14496033f85e994c52a
vishal <walavishal3064@gmail.com>
2026-04-29 11:51:56 +0530

    Project Setup with Database connection.

---
+++ app/core/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str
+    DEBUG: bool = False
+
+    DATABASE_URL: str
+
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
---
+++ app/main.py
@@ -0,0 +1,4 @@
+from fastapi import FastAPI
+from app.core.config import settings
+
+app = FastAPI(title=settings.PROJECT_NAME)
